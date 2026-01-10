631ab3c7df686b609a7d0f993a38d087fe4dfeb3
bochen <tccbabc@gmail.com>
2026-01-10 13:46:37 +0100

    modificacion de user-data.sh

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,36 +1,22 @@
 #!/bin/bash
 set -e
 
-# 1. Update system
 apt-get update -y
+apt-get install -y docker.io git curl
 
-# 2. Install required packages
-apt-get install -y \
-  docker.io \
-  git \
-  curl
-
-# 3. Enable and start Docker
 systemctl enable docker
 systemctl start docker
 
-# 4. Install docker-compose plugin
 mkdir -p /usr/local/lib/docker/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v2.25.0/docker-compose-linux-x86_64 \
   -o /usr/local/lib/docker/cli-plugins/docker-compose
 chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
 
-# 5. Allow ubuntu user to run docker
 usermod -aG docker ubuntu
 
-# 6. Switch to ubuntu user and deploy the project
 su - ubuntu <<'EOF'
 cd /home/ubuntu
-
-
 git clone https://github.com/tccbabc/CC-proyecto-2025-2026.git project
 cd project
-
-# Start the application (same as local)
 docker compose up -d --build
 EOF
